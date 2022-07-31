edf55d12e33319425c2ad1f4699cd831391938e9
ashrafur22 <ashrafurrahman22@gmail.com>
2022-07-31 23:48:19 +0600

    updated

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Customer from '../../Components/Customer';
+import Services from '../../Components/Services';
 import Navbar from '../Shared/Navbar';
 
 const Home = () => {
     return (
-        <div>
+        <div style={{fontFamily:"poppins", letterSpacing:"2px"}}>
             <Navbar></Navbar>
            <Customer></Customer>
+           <Services></Services>
         </div>
     );
 };
